9f4a42c5158ac30fe8674c98661a5536f184f032
HobbitJack <jruteg@gmail.com>
2022-11-07 14:30:42 -0500

    WIP 1.0.1

--- Plugin.cs
+++ Plugin.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 namespace Hardmode
 {
-    [BepInPlugin("org.hobbitjack.plugins.hardmode", "Cold Waters: Hardmode", "1.0.0")]
+    [BepInPlugin("org.hobbitjack.plugins.hardmode", "Cold Waters: Hardmode", "1.0.1")]
     [BepInProcess("ColdWaters.exe")]
     public class HardmodePlugin : BaseUnityPlugin
     {
+
+        //static GameObject[] mirrorcontacts { get; set; }
+        //static Vector3[] mirrorcontactpositions { get; set; }
         private void Awake()
         {
             // Plugin startup logic
@@ -20,6 +23,8 @@ namespace Hardmode
         [HarmonyPostfix]
         static void OpenTacMapOnStart(ref MissionManager __instance)
         {
+            Torpedo.Torpedoes.Clear();
+
             __instance.uifunctions.levelloadmanager.tacticalmap.SetTacticalMap();
             UIFunctions.globaluifunctions.HUDholder.SetActive(true);
         }
@@ -28,7 +33,7 @@ namespace Hardmode
         [HarmonyPrefix]
         static bool Disable3D(ref TacticalMap __instance)
         {
-            string caller = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod().DeclaringType.ToString();
+            string caller = new System.Diagnostics.StackTrace().GetFrame(2).GetMethod().DeclaringType.ToString();
             if (caller == "KeybindManager" || caller == "UIFunctions")
             {
                 Traverse.Create(__instance).Field("tacMapEnabled").SetValue(false);
@@ -36,13 +41,23 @@ namespace Hardmode
             return true;
         }
 
+        [HarmonyPatch(typeof(SubmarineFunctions), "LeavePeriscopeView")]
+        [HarmonyPostfix]
+        static void PreventPeriscopeCheating()
+        {
+
+            Traverse.Create(UIFunctions.globaluifunctions.levelloadmanager.tacticalmap).Field("tacMapEnabled").SetValue(false);
+            UIFunctions.globaluifunctions.levelloadmanager.tacticalmap.SetTacticalMap();
+        }
+
         [HarmonyPatch(typeof(TacticalMap), "SetTacticalMap")]
         [HarmonyPostfix]
         static void PreventESCCheating(ref TacticalMap __instance)
         {
-            string caller = (new System.Diagnostics.StackTrace()).GetFrame(2).GetMethod().DeclaringType.ToString();
+            string caller = new System.Diagnostics.StackTrace().GetFrame(2).GetMethod().DeclaringType.ToString();
             if (caller == "ManualCameraZoom")
             {
+
                 UIFunctions.globaluifunctions.missionmanager.BringInExitMenu(false);
             }
         }
@@ -79,9 +94,88 @@ namespace Hardmode
         [HarmonyPostfix]
         static void DisplayLoudExplosionOnHit(ref Compartment __instance)
         {
-            string contactname = UIFunctions.globaluifunctions.playerfunctions.GetFullContactName(UIFunctions.globaluifunctions.playerfunctions.sensormanager.tacticalmap.mapContact[__instance.activeVessel.vesselListIndex].contactText.text, __instance.activeVessel.vesselListIndex);
-            UIFunctions.globaluifunctions.playerfunctions.PlayerMessage($"Conn, sonar: loud explosion on the bearing of {contactname}.", UIFunctions.globaluifunctions.playerfunctions.messageLogColors["Sonar"], "", false);
+            if (!__instance.activeVessel.playercontrolled)
+            {
+                string contactname = UIFunctions.globaluifunctions.playerfunctions.GetFullContactName(UIFunctions.globaluifunctions.playerfunctions.sensormanager.tacticalmap.mapContact[__instance.activeVessel.vesselListIndex].contactText.text, __instance.activeVessel.vesselListIndex);
+                if (UIFunctions.globaluifunctions.playerfunctions.sensormanager.detectedByPlayer[__instance.activeVessel.vesselListIndex] && !__instance.activeVessel.isSinking)
+                {
+                    UIFunctions.globaluifunctions.playerfunctions.PlayerMessage($"Conn, sonar: loud explosion on the bearing of {contactname}.", UIFunctions.globaluifunctions.playerfunctions.messageLogColors["Sonar"], "", true);
+                }
+            }
         }
         //END TEXT FOR LOUD EXPLOSION ON BEARING
+
+        //DRAW TA MIRROR CONTACTS
+        /*[HarmonyPatch(typeof(TacticalMap), "TacticalMapInit")]
+        [HarmonyPostfix]
+        static void InitializeMirrorContacts()
+        {
+
+            HardmodePlugin.mirrorcontacts = new GameObject[GameDataManager.enemyNumberofShips];
+            HardmodePlugin.mirrorcontactpositions = new Vector3[GameDataManager.enemyNumberofShips];
+        }
+
+        [HarmonyPatch(typeof(SensorManager), "CalculatePlayerTMA")]
+        [HarmonyPostfix]
+        static void DrawTAMirrorContacts(ref SensorManager __instance, int enemyIndex, bool detectedByPlayerWithActive)
+        {
+            if (!(UIFunctions.globaluifunctions.playerfunctions.playerVessel.databaseshipdata.towedSonarID == -1) && UIFunctions.globaluifunctions.playerfunctions.damagecontrol.CheckSubsystem("TOWED", false))
+            {
+                if (GameDataManager.enemyvesselsonlevel[enemyIndex].acoustics.playerHasDetectedWith[0] && !GameDataManager.enemyvesselsonlevel[enemyIndex].acoustics.playerHasDetectedWith[1] && !GameDataManager.enemyvesselsonlevel[enemyIndex].acoustics.playerHasDetectedWith[2] && !GameDataManager.enemyvesselsonlevel[enemyIndex].acoustics.playerHasDetectedWith[3])
+                {
+                    int passiveSonar = int.Parse($"{Traverse.Create(UIFunctions.globaluifunctions.playerfunctions.sensormanager).Method("GetSonarReadingValues", GameDataManager.enemyvesselsonlevel[enemyIndex], GameDataManager.enemyvesselsonlevel[enemyIndex].vesselai.sensordata.playerSignatureData, true).GetValue()}".Split('\n')[1]);
+                    if (!detectedByPlayerWithActive && passiveSonar <= UIFunctions.globaluifunctions.playerfunctions.sensormanager.detectionThresholds.y)
+                    {
+                        float enemyBearing = UIFunctions.globaluifunctions.GetBearingToTransform(UIFunctions.globaluifunctions.playerfunctions.playerVessel.transform, GameDataManager.enemyvesselsonlevel[enemyIndex].transform);
+                        float bearingDiff = 180f - (360f % (UIFunctions.globaluifunctions.playerfunctions.playerVessel.CurrentHeading - enemyBearing));
+                        float mirrorTABearing = (UIFunctions.globaluifunctions.playerfunctions.playerVessel.CurrentHeading + 180f) % 360f;
+                        float mirrorContactBearing = enemyBearing > mirrorTABearing ? (bearingDiff - mirrorTABearing) % 360f : (bearingDiff + mirrorTABearing) % 360f;
+                        GameDataManager.playervesselsonlevel[0].acoustics.sensorNavigator.transform.LookAt(GameDataManager.enemyvesselsonlevel[enemyIndex].transform.position);
+                        GameDataManager.playervesselsonlevel[0].acoustics.sensorNavigator.transform.Translate(Vector3.forward * __instance.solutionRangeErrors[enemyIndex] * GameDataManager.inverseYardsScale * __instance.rangeToContacts[enemyIndex]);
+                        GameDataManager.playervesselsonlevel[0].acoustics.sensorNavigator.transform.localPosition = Vector3.zero;
+                        if (UIFunctions.globaluifunctions.playerfunctions.sensormanager.solutionQualityOfContacts[enemyIndex] > UIFunctions.globaluifunctions.playerfunctions.sensormanager.tacticalmap.qualityToDrawTails)
+                        {
+                            UIFunctions.globaluifunctions.playerfunctions.sensormanager.solutionQualityOfContacts[enemyIndex] = UIFunctions.globaluifunctions.playerfunctions.sensormanager.tacticalmap.qualityToDrawTails - 1f;
+                        }
+                    }
+                }
+            }
+        }
+        static void RefreshContact(TacticalMap tacticalMap, Vessel activeVessel)
+        {
+            int i = activeVessel.vesselListIndex;
+            if (activeVessel.isSinking || activeVessel.isCapsizing)
+            {
+                return;
+            }
+            else
+            {
+                if (tacticalMap.sensormanager.detectedByPlayer[i])
+                {
+                    HardmodePlugin.mirrorcontacts[i].transform.localPosition = new Vector3(tacticalMap.sensormanager.enemyPositions[i].x * tacticalMap.zoomFactor, tacticalMap.sensormanager.enemyPositions[i].z * tacticalMap.zoomFactor, -5f);
+                    Quaternion rotation = Quaternion.identity;
+                    if (tacticalMap.sensormanager.solutionQualityOfContacts[i] < tacticalMap.qualityToCourse)
+                    {
+                        if (tacticalMap.sensormanager.identifiedByPlayer[i])
+                        {
+                            tacticalMap.mapContact[i].shipDisplayIcon.sprite = tacticalMap.sensormanager.sonarPaintImages[tacticalMap.sensormanager.shipTypes[i]];
+                        }
+                        else
+                        {
+                            tacticalMap.mapContact[i].shipDisplayIcon.sprite = tacticalMap.sensormanager.sonarPaintImages[0];
+                        }
+                    }
+                    else
+                    {
+                        Vector3 eulerAngles = activeVessel.transform.eulerAngles;
+                        rotation = Quaternion.Euler(0f, 180f, eulerAngles.y);
+                        tacticalMap.mapContact[i].shipDisplayIcon.sprite = tacticalMap.sensormanager.sonarPaintImages[5];
+                    }
+                    tacticalMap.mapContact[i].shipDisplayIcon.transform.rotation = rotation;
+                }
+            }
+        }
+        //END DRAW TA MIRROR CONTACTS
+        */
     }
 }
---
+++ bin/Debug/net35/README.txt
@@ -0,0 +1,27 @@
+COLD WATERS: HARDMODE
+Developed by HobbitJack
+Version: 1.0.0
+
+This plugin is a difficulty modifier for Cold Waters. 
+It is designed for advanced players seeking a realistic experience above and beyond that found by default in the game.
+Even though this is a difficulty plug-in, it is designed for use on the Realistic config setting, due to Elite adversly affecting vessel stats and sensor comparisons
+Due to using BepInEx/Harmony, it is theoretically possibled that this plugin will work as an add-on for any mod of your choosing, however please note that it has only been tested with DotMod and with only the base game.
+
+Features:
+ - Disables 3D view, except for periscope (and pause menu, but we can pretend that works just fine)
+ - Disables autoclassification of vessels, regardless of difficulty setting
+ - Game will display the type of a torpedo when the icon is clicked on the map
+ - Game will inform the player of hits on enemy vessels
+ 
+Installation:
+1. Download BepInEx V5.4.21x86 from the BepInEx GitHub page: https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x86_5.4.21.0.zip
+2. Extract all items except the changelog to your Cold Waters directory (i.e. the BepInEx folder should be next to ColdWaters_Data)
+3. Run the game. You should get a black screen.
+4. Go to BepInEx/config/BepInEx.cfg, and under [Entry.Preloader], find Type = Application, and change that to Type = MonoBehaviour.
+5. Run the game. It should launch normally.
+6. Copy the CWHardmode.dll to BepInEx/plugins.
+7. Launch the game. The plugin should be properly installed.
+
+Note that this is a first version of this release; any issues found should be reported directly to HobbitJack.
+
+Have fun!
